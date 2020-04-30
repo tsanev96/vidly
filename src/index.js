c3ef7a03b126cfa4fce65ee9ca6a41f4073b2401
tsanev96 <tsanev96@gmail.com>
2020-04-30 12:23:02 +0300

    rental route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals')
 const app = express();
 
 app.use(express.json());
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening on port 3000..'))
