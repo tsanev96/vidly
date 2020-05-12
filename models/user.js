fb2b48799b60f0d99d8ff8f0438456c5642dd831
tsanev96 <tsanev96@gmail.com>
2020-05-12 15:41:41 +0300

    winston logging errors in files/console fixed, older version used

--- models/user.js
+++ models/user.js
@@ -43,4 +43,5 @@ function validateUser(user) {
 }
 
 exports.User = User;
+exports.userSchema = userSchema;
 exports.validate = validateUser;
--- startup/logging.js
+++ startup/logging.js
@@ -1,18 +1,20 @@
 const winston = require('winston');
 require('winston-mongodb');
 require('express-async-errors');
-// TODO: winston mongodb, log errors in the console
 
 module.exports = function () {
-  process.on('uncaughtException', (ex) => {
-    winston.error(ex.message, ex);
-    process.exit(1);
-  });
+  winston.handleExceptions(
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' }),
+    new winston.transports.Console({ colorize: true, prettyPrint: true })
+  );
 
   process.on('unhandledRejection', (ex) => {
-    winston.error(ex.message, ex);
-    process.exit(1);
+    throw ex;
   });
 
-  winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+  winston.add(winston.transports.File, { filename: 'logfile.log' });
+  winston.add(winston.transports.MongoDB, {
+    db: 'mongodb://localhost/vidly',
+    level: 'info'
+  });
 };
